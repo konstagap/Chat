fe8270167341b08d442c9ef5331c14b2bf643a4d
konstagap <konstagapov123@gmail.com>
2019-12-04 13:05:16 -0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
+
+function App() {
+  return (
+    <Router>
+      <Route exact path="/" component={Join} />
+      <Route exact path="/chat" component={Chat} />
+    </Router>
+  );
+}
+export default App;
---
+++ client/src/components/Messages/Message/Message.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import "./Message.css";
+
+import ReactEmoji from "react-emoji";
+
+let time = new Date().toLocaleTimeString();
+
+const Message = ({ message: { text, user }, name }) => {
+  let isSentByCurrentUser = false;
+
+  const trimmedName = name.trim().toLowerCase();
+
+  if (user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
+
+  return isSentByCurrentUser ? (
+    <div className="messageContainer justifyEnd">
+      <p className="sentText pr-10">
+        {trimmedName} {time}
+      </p>
+      <div className="messageBox backgroundBlue">
+        <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+      </div>
+    </div>
+  ) : (
+    <div className="messageContainer justifyStart">
+      <div className="messageBox backgroundLight">
+        <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+      </div>
+      <p className="sentText pl-10 ">
+        {user} {time}
+      </p>
+    </div>
+  );
+};
+
+export default Message;
